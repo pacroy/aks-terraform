86aa24c748b6db0e98d7fa2de0ac94723d7f9786
Par <pacroy@gmail.com>
2020-04-16 13:19:13 +0700

    Fixed kube_config variable

--- cert-manager/variables.tf
+++ cert-manager/variables.tf
@@ -1,3 +1,10 @@
-variable "kube_config" {}
+variable "kube_config" {
+  type = object({
+    host                    = string
+    client_certificate      = string
+    client_key              = string
+    cluster_ca_certificate  = string
+  })
+}
 variable "namespace" {}
 
