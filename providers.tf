42b3d44bd683fc9b6110eeb4a8d09fe35c9ad0e7
Par <chairat.onyaem@exxonmobil.com>
2020-04-16 15:10:31 +0700

    Remove help provider from root

--- providers.tf
+++ providers.tf
@@ -11,20 +11,3 @@ terraform {
   backend "azurerm" {
   }
 }
-
-provider "helm" {
-  version = "~> 1.1"
-  kubernetes {
-    host = azurerm_kubernetes_cluster.main.kube_config.0.host
-
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate)
-    load_config_file       = false
-  }
-}
-
-data "helm_repository" "stable" {
-  name = "stable"
-  url  = "https://kubernetes-charts.storage.googleapis.com"
-}
