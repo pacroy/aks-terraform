ddf474cec9720bcc2fc974b602989c11fc36a1ad
Par <pacroy@gmail.com>
2020-04-16 16:13:12 +0700

    Restored to module porviders

--- cert-manager/main.tf
+++ cert-manager/main.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "cert-manager" {
   name       = "cert-manager"
-  repository = "stable" #data.helm_repository.stable.name
+  repository = data.helm_repository.stable.name
   chart      = "cert-manager"
   namespace  = var.namespace
 }
