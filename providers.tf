4100a534e34dc5cb4a8f5b45aa986807fc052e5b
Par <pacroy@gmail.com>
2020-04-16 14:23:37 +0700

    Fixed aks kube_config first element

--- providers.tf
+++ providers.tf
@@ -15,11 +15,11 @@ terraform {
 provider "helm" {
   version = "~> 1.1"
   kubernetes {
-    host = azurerm_kubernetes_cluster.main.kube_config.host
+    host = azurerm_kubernetes_cluster.main.kube_config.0.host
 
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config.client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config.client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config.cluster_ca_certificate)
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate)
     load_config_file       = false
   }
 }
