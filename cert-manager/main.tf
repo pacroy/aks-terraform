0d0efa2987ebf2533aed7eaba438ef455207a9db
Par <pacroy@gmail.com>
2020-04-16 21:25:09 +0700

    Temporary removed cert-manager as cainjector is CrashLoopBackOff

--- cert-manager/main.tf
+++ cert-manager/main.tf
@@ -1,7 +1,7 @@
-resource "helm_release" "cert-manager" {
-  name       = "cert-manager"
-  repository = data.helm_repository.jetstack.name
-  chart      = "cert-manager"
-  namespace  = var.namespace
-  version    = "v0.15.0-alpha.0" #This version includes CRDs in the chart
-}
+# resource "helm_release" "cert-manager" {
+#   name       = "cert-manager"
+#   repository = data.helm_repository.jetstack.name
+#   chart      = "cert-manager"
+#   namespace  = var.namespace
+#   version    = "v0.15.0-alpha.0" #This version includes CRDs in the chart
+# }
