60827085207ff9bc49942cc8513189eab57e2d99
Par <pacroy@gmail.com>
2020-04-16 15:56:39 +0700

    Re-added helm modules without data variable

--- cert-manager/main.tf
+++ cert-manager/main.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "cert-manager" {
   name       = "cert-manager"
-  repository = data.helm_repository.stable.name
+  repository = "stable" #data.helm_repository.stable.name
   chart      = "cert-manager"
   namespace  = var.namespace
 }
