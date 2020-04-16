e9bba82b616fc04a4868088d2cc5a821733f6324
Par <chairat.onyaem@exxonmobil.com>
2020-04-16 12:25:10 +0700

    Added helm/cert-manager

---
+++ cert-manager/variables.tf
@@ -0,0 +1,3 @@
+variable "kube_config" {}
+variable "namespace" {}
+
--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-    version       = "~> 2.4.0"
+    version       = "=2.4.0"
     subscription_id = var.subscription_id
     tenant_id       = var.tenant_id
     client_id       = var.client_id
@@ -11,3 +11,13 @@ terraform {
   backend "azurerm" {
   }
 }
+
+# provider "kubernetes" {
+#   host = azurerm_kubernetes_cluster.main.kube_config.host
+
+#   client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config.client_certificate)
+#   client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config.client_key)
+#   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config.cluster_ca_certificate)
+#   load_config_file       = false
+#   version                = "=1.10.0"
+# }
