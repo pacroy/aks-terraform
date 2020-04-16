57dee9919c5f5a013ceeb94a970021756877027b
Par <chairat.onyaem@exxonmobil.com>
2020-04-16 16:13:12 +0700

    Restored to module porviders

--- nginx-ingress/main.tf
+++ nginx-ingress/main.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "nginx-ingress" {
   name       = "nginx-ingress"
-  repository = "stable" #data.helm_repository.stable.name
+  repository = data.helm_repository.stable.name
   chart      = "nginx-ingress"
   namespace  = "kube-system"
 }
