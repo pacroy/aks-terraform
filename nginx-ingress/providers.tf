7dbba17ba8f7cb142af351fc9d49be5649a262e4
Par <pacroy@gmail.com>
2020-04-16 16:06:06 +0700

    Use root providers

--- nginx-ingress/providers.tf
+++ nginx-ingress/providers.tf
@@ -1,14 +1,14 @@
-provider "helm" {
-  version = "~> 1.1"
-  kubernetes {
-    host = var.kube_config.host
+# provider "helm" {
+#   version = "~> 1.1"
+#   kubernetes {
+#     host = var.kube_config.host
 
-    client_certificate     = base64decode(var.kube_config.client_certificate)
-    client_key             = base64decode(var.kube_config.client_key)
-    cluster_ca_certificate = base64decode(var.kube_config.cluster_ca_certificate)
-    load_config_file       = false
-  }
-}
+#     client_certificate     = base64decode(var.kube_config.client_certificate)
+#     client_key             = base64decode(var.kube_config.client_key)
+#     cluster_ca_certificate = base64decode(var.kube_config.cluster_ca_certificate)
+#     load_config_file       = false
+#   }
+# }
 
 # data "helm_repository" "stable" {
 #   name = "stable"
--- providers.tf
+++ providers.tf
@@ -13,11 +13,23 @@ provider "azurerm" {
 }
 
 provider "kubernetes" {
-  host = azurerm_kubernetes_cluster.main.kube_config.0.host
+  host = azurerm_kubernetes_cluster.main.kube_config[0].host
 
   client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].client_certificate)
   client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].client_key)
   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate)
   load_config_file       = false
   version                = "~> 1.10"
+}
+
+provider "helm" {
+  version = "~> 1.1"
+  kubernetes {
+    host = azurerm_kubernetes_cluster.main.kube_config[0].host
+
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate)
+    load_config_file       = false
+  }
 }
