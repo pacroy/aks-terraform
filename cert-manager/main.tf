8ff1c2404e18fc5bbfdae206fbad7b23331fd8b0
Par <pacroy@gmail.com>
2020-04-17 22:08:11 +0700

    Fixed failed to download pacroy/cert-manager

--- cert-manager/main.tf
+++ cert-manager/main.tf
@@ -1,9 +1,10 @@
 resource "helm_release" "cert-manager" {
-  name       = "cert-manager"
-  repository = data.helm_repository.main.name
-  chart      = "cert-manager"
-  namespace  = var.namespace
-  version    = "0.14.2"
+  name          = "cert-manager"
+  repository    = data.helm_repository.main.metadata[0].url
+  chart         = "cert-manager"
+  namespace     = var.namespace
+  version       = "0.14.2"
+  force_update  = true
 
   set {
     name  = "email"
--- nginx-ingress/main.tf
+++ nginx-ingress/main.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "nginx-ingress" {
   name       = "nginx-ingress"
-  repository = data.helm_repository.main.name
+  repository = data.helm_repository.main.metadata[0].name
   chart      = "nginx-ingress"
   namespace  = "kube-system"
 }
