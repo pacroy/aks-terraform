8e142921350655e3cf8a9a391a1812eed2f9ead2
Par <pacroy@gmail.com>
2020-04-17 22:15:08 +0700

    Updated helm provider to 1.1

--- cert-manager/main.tf
+++ cert-manager/main.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "cert-manager" {
   name          = "cert-manager"
-  repository    = data.helm_repository.main.metadata[0].url
+  repository    = data.helm_repository.main.metadata[0].name
   chart         = "cert-manager"
   namespace     = var.namespace
   version       = "0.14.2"
