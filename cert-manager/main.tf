ee21a3326369837c61e3d73fabd7861e6b2b5fa4
Par <pacroy@gmail.com>
2020-05-23 13:17:18 +0000

    Install cert-manager directly from Jetstack

--- cert-manager/main.tf
+++ cert-manager/main.tf
@@ -3,11 +3,17 @@ resource "helm_release" "cert-manager" {
   repository    = data.helm_repository.main.metadata[0].name
   chart         = "cert-manager"
   namespace     = var.namespace
+  version       = "0.15.0"
   force_update  = true
 
   set {
     name  = "email"
     value = var.email
+  }
+
+  set {
+    name  = "installCRDs"
+    value = true
   }
 
 }
