821ff0c96c6ca9ffebaa8d3a1045faa0d7516272
Par <pacroy@gmail.com>
2020-08-04 10:20:10 +0700

    Add missing namespace to create implicit dependency

--- cluster_issuer/main.tf
+++ cluster_issuer/main.tf
@@ -3,6 +3,7 @@ resource "helm_release" "cluster-issuer" {
   repository    = "https://raw.githubusercontent.com/pacroy/helm-repo/master"
   chart         = "cluster-issuer"
   version       = "1.0.0"
+  namespace     = var.namespace
 
   set {
     name  = "email"
