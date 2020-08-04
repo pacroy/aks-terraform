e1e85628e2c890313a2b8d5b12104a1eb1d6ac1d
Par <pacroy@gmail.com>
2020-08-04 10:04:33 +0700

    Bump aks to v1.17.9

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "azurerm_kubernetes_cluster" "main" {
   location            = var.location
   resource_group_name = azurerm_resource_group.main.name
   dns_prefix          = var.aks_cluster_name
-  kubernetes_version  = "1.17.7"
+  kubernetes_version  = "1.17.9"
   tags  = var.tags
 
   default_node_pool {
