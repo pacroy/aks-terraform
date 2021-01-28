fb023c47341016571b0b285cb184dc4da4cb6f35
Par <pacroy@gmail.com>
2021-01-28 14:19:46 +0700

    Revert "Adopt required_providers"
    This reverts commit 47867b30ba5ff9cac12747ce439eb6f9941301d9.

--- cert_manager/providers.tf
+++ cert_manager/providers.tf
@@ -1,14 +1,11 @@
 provider "helm" {
   version = "~> 1.3.0"
+  kubernetes {
+    host = var.kube_config.host
 
-  required_providers {
-    kubernetes = {
-      host = var.kube_config.host
-
-      client_certificate     = base64decode(var.kube_config.client_certificate)
-      client_key             = base64decode(var.kube_config.client_key)
-      cluster_ca_certificate = base64decode(var.kube_config.cluster_ca_certificate)
-      load_config_file       = false
-    }
+    client_certificate     = base64decode(var.kube_config.client_certificate)
+    client_key             = base64decode(var.kube_config.client_key)
+    cluster_ca_certificate = base64decode(var.kube_config.cluster_ca_certificate)
+    load_config_file       = false
   }
 }
