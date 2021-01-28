54c7d9b5b4e58c79584be105fd57437beb108552
Par <pacroy@gmail.com>
2021-01-28 14:17:18 +0700

    Adopt required_providers

--- providers.tf
+++ providers.tf
@@ -1,23 +1,25 @@
 terraform {
   backend "azurerm" {
   }
-}
+  
+  required_providers {
+    azurerm = {
+      version       = "~> 2.44.0"
+      subscription_id = var.subscription_id
+      tenant_id       = var.tenant_id
+      client_id       = var.client_id
+      client_secret   = var.client_secret
+      features {}
+    }
 
-provider "azurerm" {
-    version       = "~> 2.44.0"
-    subscription_id = var.subscription_id
-    tenant_id       = var.tenant_id
-    client_id       = var.client_id
-    client_secret   = var.client_secret
-    features {}
-}
+    kubernetes = {
+      host = azurerm_kubernetes_cluster.main.kube_config[0].host
 
-provider "kubernetes" {
-  host = azurerm_kubernetes_cluster.main.kube_config[0].host
-
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate)
-  load_config_file       = false
-  version                = "~> 1.13.0"
+      client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].client_certificate)
+      client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].client_key)
+      cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate)
+      load_config_file       = false
+      version                = "~> 1.13.0"
+    }
+  }
 }
